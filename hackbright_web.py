f17910b8e9001d9a01dad028cc9ab4542f26de35
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-23 15:53:20 -0800

    add user input form to add new student; add routes and templates

--- hackbright_web.py
+++ hackbright_web.py
@@ -11,12 +11,40 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{acct} is the GitHub account for {first} {last}".format(
-        acct=github, first=first, last=last)
+    return render_template('student_info.html', github=github,
+                           first=first, last=last)
+
+
+@app.route("/new-student-form")
+def add_student_form():
+    """Show form for searching student"""
+
+    return render_template("new_student.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def display_new_student():
+    """Show information about a new student."""
+
+    github = request.form.get('github')
+    first = request.form.get('first')
+    last = request.form.get('last')
+
+    hackbright.make_new_student(first, last, github)
+
+    return render_template('new_student_confirmation.html', github=github)
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching student"""
+
+    return render_template("student_search.html")
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
